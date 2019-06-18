515059ea9de937ab1ef2a4e70e729ab72befd017
Eugenia Leung <eugenia_leung316@yahoo.com>
2019-06-17 21:22:29 -0700

    initial commit

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,52 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Solution 6
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
+
+// Solution 1
+// function reverse(str) {
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join("");
+// }
+
+// Solution 2
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+// Solution 3
+// function reverse(str) {
+//     var reversed = "";
+//     for (var i = str.length - 1; i > -1; i--) {
+//       reversed += str[i];
+//     }
+//     return reversed;
+//     console.log(reversed);
+//   }
+
+// Solution 4
+// function reverse(str) {
+//     let reversed = "";
+
+//     for (let character of str) {
+//       reversed = character + reversed;
+//       console.log(reversed);
+//     }
+//     return reversed;
+//   }
+
+// Solution 5
+// function reverse(str) {
+//     return str.split("").reduce((reversed, character) => {
+//       return character + reversed;
+//     }, "");
+//   }

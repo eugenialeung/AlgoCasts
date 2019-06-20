2bc8dce21489b6642389d1f56e50f6ff0204804b
Eugenia Leung <eugenia_leung316@yahoo.com>
2019-06-20 13:39:13 -0700

    capitalize solution 1

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,32 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
+
+// Solution 1
+// function chunk(array, size) {
+//     const chunked = [];
+//     for (let element of array) {
+//       const last = chunked[chunked.length - 1];
+
+//       // push a new chunk into chunked
+//       if (!last || last.length === size) {
+//         chunked.push([element]);
+//       } else {
+//           // chunk not yet full
+//         last.push(element);
+//       }
+//     }
+//     return chunked;
+//   }
